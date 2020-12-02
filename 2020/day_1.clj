ea8815c8dfd5f9aad0383efe4156296a2a8f13a3
Max Deineko <max.deineko@gmail.com>
2020-12-02 04:39:33 +0100

    AoC 2020 Day 1

---
+++ 2020/day_1.clj
@@ -0,0 +1,28 @@
+(ns madhat.adventofcode.day-one
+  (:require [clojure.string :as str])
+  (:require [clojure.math.combinatorics :as combo]))
+
+
+;; Day 1
+
+
+(def input (slurp "day_1a_input.txt"))
+
+
+(def input-seq
+  (as-> input v
+    (str/split v #"\s+")
+    (filter #(not (= "" %)) v)
+    (map read-string v)))
+
+
+;; set combinations length to 2 or 3 depending on task part
+(as-> input-seq v
+  (combo/combinations v 3)
+  (filter #(= 2020 (apply + %)) v)
+  (first v)
+  (apply * v))
+
+
+;; TODO: map cider-pprint-eval-last-sexp-to-comment in doom
+;; => 130933530
