c38433b0984b84117f2a8d387c0ffd3e62d0a9a0
Max Deineko <max.deineko@gmail.com>
2020-12-03 07:38:47 +0100

    Day 3 cosmetics

--- 2020/day_3.clj
+++ 2020/day_3.clj
@@ -3,12 +3,11 @@
 
 
 (defn find-trees-in
-  "Given a line, returns a set of 0-based indices of tree locations"
+  "Given a line, returns the set of zero-based indices of its tree locations"
   [line]
   (->> line
        (keep-indexed #(when (= \# %2) %1))
        (into #{})))
-
 
 (defn input->topo [input]
   (let [lines (str/split input #"\n+")]
@@ -16,7 +15,10 @@
      :height (count lines)
      :lines  (map find-trees-in lines)}))
 
-(defn move [dx dy topo]
+
+(defn move
+  "Returns number of trees encountered when moving through given topography along given vector"
+  [dx dy topo]
   (loop [x         0
          y         0
          trees-cnt 0]
@@ -26,20 +28,22 @@
             new-cnt     (if (trees-coors (mod x (:width topo))) (inc trees-cnt) trees-cnt)]
         (recur (+ x dx) (+ y dy) new-cnt)))))
 
+
+;; Part 1
+
 (def topo
   (->>
    "day_3_input.txt"
    (slurp)
    (input->topo)))
 
-;; Part 1
-
 (move 3 1 topo) ;; => 280
+
 
 ;; Part 2
 
 (def slopes [[1 1] [3 1] [5 1] [7 1] [1 2]])
 
-(defn cnt-trees [dx dy] (move dx dy topo))
+(defn count-trees [dx dy] (move dx dy topo))
 
-(apply * (map (partial apply cnt-trees) slopes)) ;; => 4355551200
+(apply * (map (partial apply count-trees) slopes)) ;; => 4355551200
