08175718ac81bd9a938844174d54a2d86c2c3a3d
Max Deineko <max.deineko@gmail.com>
2020-12-05 06:46:28 +0100

    Day 5

--- 2020/day_5.clj
+++ 2020/day_5.clj
@@ -3,7 +3,8 @@
    [clojure.string :as str]
    #_[clojure.set :as set]
    #_[clojure.algo.generic.functor :as gf :refer [fmap]]
-   #_[clojure.math.combinatorics :as combo]))
+   #_[clojure.math.combinatorics :as combo]
+   #_[clojure.zip :as zip]))
 
 (defn split-input [input]
   (str/split input #"\R"))
@@ -23,10 +24,27 @@
     "day_5_input.txt"
     slurp
     split-input
-    (map parse))))
+    (map parse)
+    (into #{}))))
 
 (time
  (def part-1-answer
    (apply max (map (partial apply #(+ (* 8 %1) %2)) nums))))
 
 part-1-answer
+
+nums
+
+(defn zip [& colls]
+  (partition (count colls) (apply interleave colls)))
+
+(def part-2-answer)
+
+(for [x (range 256)
+      y (range 8)
+      :when (not (contains? nums [x y]))
+      :when (contains? nums [(dec x) y])
+      :when (contains? nums [(inc x) y])]
+  [x y])
+
+part-2-answer
