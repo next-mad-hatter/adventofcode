6767a6e80b8b4d27b1b2109b914b1d07203e9920
Max Deineko <max.deineko@gmail.com>
2020-12-29 23:20:45 +0100

    Simplify and speedup day 11

--- src/aoc_2020/day_11.clj
+++ src/aoc_2020/day_11.clj
@@ -1,18 +1,16 @@
 (ns aoc-2020.day-11
   (:require [aoc-2020.util :as util]
-            [clojure.algo.generic.functor :as gf]
-            [clojure.string :as str]
-            [taoensso.tufte :as tufte :refer [p profile]]))
+            [clojure.string :as str]))
 
 (defn lines->seats [lines]
-  (let [input (into {} (for [x (range (count lines))
-                             y (range (count (first lines)))
-                             :let [s (get-in lines [x y])]
+  (let [input (into {} (for [x     (range (count lines))
+                             y     (range (count (first lines)))
+                             :let  [s (get-in lines [x y])]
                              :when (#{"L" "#"} s)]
                          [[x y] ({"L" false "#" true} s)]))
         seats (keys input)
-        occ (set (map first (filter (fn [[_ v]] true? v) input)))]
-    {:seats seats
+        occ   (set (map first (filter (fn [[_ v]] true? v) input)))]
+    {:seats    seats
      :occupied (if (empty? occ) occ (throw (Exception. "Not implemented")))}))
 
 (defn init [filename]
@@ -21,22 +19,17 @@
        (mapv #(str/split % #""))
        lines->seats))
 
-(defn new-state-of [thresh coor cnt s]
-  (cond
-    (and (not s) (= cnt 0)) [coor true]
-    (and s (>= cnt thresh)) [coor false]
-    :else nil))
+(defn change-state-of [thresh hood state coor s]
+  (let [cnt (reduce #(+ %1 (state %2)) 0 (hood coor))]
+    (when (or
+           (and (= 0 s) (= cnt 0))
+           (and (= 1 s) (>= cnt thresh))) [coor (bit-xor 1 s)])))
 
 (defn step [hood thresh state]
-  (let [occupied      (state :occupied)
-        occ-neigh-cnt (state :occ-neigh-cnt)
-        flips         (p ::flips (vec (filter some? (map (partial new-state-of thresh) (range) occ-neigh-cnt occupied))))
-        next-occ      (p ::next-occ (reduce (fn [o [k v]] (assoc o k v)) occupied flips))
-        cnt-groups    (p ::cnt-groups (group-by second (filter (comp some? hood first) flips)))
-        cnt-items     (p ::cnt-items (gf/fmap #(mapcat (comp hood first) %) cnt-groups))
-        next-cnt      (p ::next-cnt+ (reduce (fn [c k] (update c k inc)) occ-neigh-cnt (cnt-items true {})))
-        next-cnt      (p ::next-cnt- (reduce (fn [c k] (update c k dec)) next-cnt (cnt-items false {})))]
-    {:occupied next-occ, :occ-neigh-cnt next-cnt}))
+  (let [inds   (keep-indexed (partial change-state-of thresh hood state) state)
+        state  (transient state)
+        state' (reduce (fn [st [i v]] (assoc! st i v)) state inds)]
+    (persistent! state')))
 
 (defn find-fixed-point [xs]
   (->> xs
@@ -49,49 +42,40 @@
 
 (defn solve [construct-hood thresh filename]
   (let [start (init filename)
-        hood (construct-hood (start :seats))
-        tr (enumerate (start :seats))
-        hood (into {} (mapv (fn [[k v]] [(tr k) (filter some? (mapv tr v))]) hood))
-        hood (mapv second (sort-by first (into [] hood)))
-        occupied (vec (repeat (count hood) false))
-        occ-neigh-cnt (vec (repeat (count hood) 0))]
-    (->> {:occupied occupied, :occ-neigh-cnt occ-neigh-cnt}
+        hood  (construct-hood (start :seats))
+        tr    (enumerate (start :seats))
+        hood  (map (fn [[k v]] [(tr k) (filter some? (mapv tr v))]) hood)
+        hood  (mapv (comp seq second) (sort-by first hood))
+        state (vec (repeat (count hood) 0))]
+    (->> state
          (iterate (partial step hood thresh))
          (take 100000)
-         (map :occupied)
          find-fixed-point
-         (filter true?)
-         count)))
+         (reduce + 0))))
 
 ;;
 ;; Part 1
 ;;
 
 (def eight-dirs
-  (for [x [-1 0 1]
-        y [-1 0 1]
+  (for [x     [-1 0 1]
+        y     [-1 0 1]
         :when (not= [x y] [0 0])]
     [x y]))
 
 (defn construct-hood-1 [seats]
-  (let [mx (apply max (map first seats))
-        my (apply max (map last seats))
-        all (into {} (map (fn [coors] [coors (set (map #(mapv + coors %) eight-dirs))]) seats))]
-    (gf/fmap #(set (filter (fn [[x y]] (and (<= 0 x mx) (<= 0 y my))) %)) all)))
+  (map (fn [coors] [coors (map #(mapv + coors %) eight-dirs)]) seats))
 
 (defn part-1 [filename]
   (solve construct-hood-1 4 filename))
 
-(comment
-  (time (part-1 "2020/day_11_small.txt")))
+(time (part-1 "2020/day_11_small.txt"))
 ;; => 6
 
 (time (part-1 "2020/day_11_test.txt"))
 ;; => 37
 
-;; Takes appr. 1.5 sec w/ i5-7200U or ? secs w/ i5-8265U
-(comment
-  (time (part-1 "2020/day_11_input.txt")))
+(time (part-1 "2020/day_11_input.txt"))
 ;; => 2275
 
 ;;
@@ -99,9 +83,9 @@
 ;;
 
 (defn construct-hood-2 [seats]
-  (let [seats     (set seats)
-        mx        (apply max (map first seats))
-        my        (apply max (map last seats))
+  (let [seats (set seats)
+        mx    (apply max (map first seats))
+        my    (apply max (map last seats))
 
         move      (fn [dir] (fn [from] (mapv + from dir)))
         ray       (fn [from dir] (rest (iterate (move dir) from)))
@@ -110,8 +94,8 @@
                                   (contains? seats [x y])))
         neighbour (fn [coors dir] (util/find-first stop-at (ray coors dir)))
 
-        looks-at  (fn [coors] (set (filter some? (map #(->> % (neighbour coors) seats) eight-dirs))))]
-    (into {} (map (fn [k] [k (looks-at k)]) seats))))
+        looks-at (fn [coors] (filter some? (map #(->> % (neighbour coors) seats) eight-dirs)))]
+    (map (fn [k] [k (looks-at k)]) seats)))
 
 (defn part-2 [filename]
   (solve construct-hood-2 5 filename))
@@ -119,33 +103,6 @@
 (time (part-2 "2020/day_11_test.txt"))
 ;; => 26
 
-;; Takes appr. 1.5 secs w/ i5-7200U or 1.3 secs w/ i5-8265U
-(comment
-  (time (part-2 "2020/day_11_input.txt")))
+(time (part-2 "2020/day_11_input.txt"))
 ;; => 2121
 
-;;
-;; Until we're done tinkering, we'll leave this here
-;;
-
-(tufte/add-basic-println-handler! {})
-
-(comment
-  (time
-   (profile
-    {}
-    (ffirst
-     (mapv identity
-           (let [start (init "2020/day_11_input.txt")
-                 hood (construct-hood-2 (start :seats))
-                 thresh 5
-                 tr (enumerate (start :seats))
-                 hood (into {} (mapv (fn [[k v]] [(tr k) (filter some? (mapv tr v))]) hood))
-                 hood (mapv second (sort-by first (into [] hood)))
-                 occupied (vec (repeat (count hood) false))
-                 occ-neigh-cnt (vec (repeat (count hood) 0))]
-             (->> {:occupied occupied, :occ-neigh-cnt occ-neigh-cnt}
-                  (iterate (partial step hood thresh))
-                  (take 30)
-                  (doall)
-                  first)))))))
